78382a955e5a9c1d9f7ac06310cb9cf1a27fbe3f
mc <iammay1234@users.noreply.github.com>
2020-09-11 16:12:34 -0500

    just linking of prev next

--- assets/js/instdebate.js
+++
@@ -1,7 +0,0 @@
-<script>
-var idcomments_acct = 'b5e0bde061566050b850f31837ef35bd';
-var idcomments_post_id;
-var idcomments_post_url;
-</script>
-<span id="IDCommentsPostTitle" style="display:none"></span>
-<script type='text/javascript' src='https://www.intensedebate.com/js/genericCommentWrapperV2.js'></script>
