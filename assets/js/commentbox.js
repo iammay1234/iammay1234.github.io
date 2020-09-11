e5b6b27ded339ddeab05ee0e43d2397d8b99c35f
mc <iammay1234@users.noreply.github.com>
2020-09-10 22:52:11 -0500

    move commentbox js import to its own file

--- assets/js/commentbox.js
+++ assets/js/commentbox.js
@@ -1,2 +1,9 @@
-const commentBox = require('commentbox.io');
+
+
+<!-- add commentBox setup by May -->
+<script src="https://unpkg.com/commentbox.io/dist/commentBox.min.js"></script>
+
+import commentBox from 'commentbox.io';
+
+// const commentBox = require('commentbox.io');
 commentBox('5642947798237184-proj');
