7e8e274f37831c27b4778b8c269bbfb0eeec0f17
mc <iammay1234@users.noreply.github.com>
2020-09-10 22:57:43 -0500

    test again

--- assets/js/commentbox.js
+++ assets/js/commentbox.js
@@ -1,8 +1,3 @@
-
-
-<!-- add commentBox setup by May -->
-<script src="https://unpkg.com/commentbox.io/dist/commentBox.min.js"></script>
-
 import commentBox from 'commentbox.io';
 
 commentBox('5642947798237184-proj');
